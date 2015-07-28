79e9c4112e19e6d271fe215b663d9e3c808b9a3b
pollak <amit.pollak@iprosis.com>
2015-07-28 09:19:10 +0300

    Master
    Master

---
+++ res/js/component.js
@@ -0,0 +1,131 @@
+sap.ui.commons.RangeSlider.extend("com.iprosis.sapui5.RangeSlider", {
+	initDesignStudio: function() {
+		this.attachChange(function() {
+			this.fireDesignStudioPropertiesChanged(["value"]);
+			this.fireDesignStudioPropertiesChanged(["value2"]);
+			this.fireDesignStudioEvent("onChange");
+		});
+	},
+	renderer: {}
+});
+
+sap.ui.commons.Slider.extend("com.iprosis.sapui5.Slider", {
+	initDesignStudio: function() {
+		this.attachChange(function() {
+			this.fireDesignStudioPropertiesChanged(["value"]);
+			this.fireDesignStudioEvent("onChange");
+		});
+	},
+	renderer: {}
+});
+
+sap.designstudio.sdk.Component.subclass("com.iprosis.sapui5.AutoComplete", function() {
+
+	var dataResultSet = null;
+	var maxItems = null;
+	var maxPopupItems = null;
+	var displayKey = null;
+	var selectedValue = null;
+	var that = this;
+	var Data = null;
+	this.oAuto = null;
+
+	this.init = function() {
+		
+		this.$().click(function() {
+			that.fireEvent("onclick");
+		});
+		
+		if (this._alive){
+			return;
+		} else {
+			
+			currentDiv = "DIV_" + this.$().attr('id');
+			
+			//Create a AutoComplete control and fill the items
+			this.oAuto = new sap.ui.commons.AutoComplete({
+				tooltip: "Enter a name",
+				maxPopupItems: 5,
+				displaySecondaryValues: true,
+				items: {
+					path: "/",
+					template: new sap.ui.core.ListItem({text: "{text}", additionalText: "{key}"})
+				},
+				change : function() {  
+    			that.fireEvent("onChange"); 
+    			that.firePropertiesChanged(["SelectedValue"]);  
+			} 
+			});
+			
+			this.$().html('<div id="' + currentDiv + '" class="sapUiBody"> ');
+			
+			this.oAuto.placeAt(currentDiv);
+						
+			this._alive = true;
+			
+
+		}
+	};
+
+	this.afterUpdate = function() {
+		this.insertData();
+		selectedValue = this.oAuto.getValue();
+		
+	};
+	
+	this.insertData = function() {
+		//Initialize a JSON Model
+		jQuery.sap.require("sap.ui.model.json.JSONModel");
+		var oModel = new sap.ui.model.json.JSONModel();
+		oModel.setData(dataResultSet.dimensions[0].members);
+		oModel.setSizeLimit(maxItems);
+		this.oAuto.setMaxPopupItems(maxPopupItems);
+		this.oAuto.setDisplaySecondaryValues(displayKey);
+		this.oAuto.setModel(oModel);
+	};
+	
+	this.DataResultSet = function(value) {
+		if(value===undefined) {
+			return dataResultSet;
+		} else {
+			dataResultSet = value;
+			return this;
+		};
+	};
+	
+	this.MaxItems = function(value) {
+		if(value===undefined) {
+			return maxItems;
+		} else {
+			maxItems = value;
+			return this;
+		};
+	};
+	
+	this.MaxPopupItems = function(value) {
+		if(value===undefined) {
+			return maxPopupItems;
+		} else {
+			maxPopupItems = value;
+			return this;
+		};
+	};
+	
+	this.DisplayKey = function(value) {
+		if(value===undefined) {
+			return displayKey;
+		} else {
+			displayKey = value;
+			return this;
+		};
+	};
+	
+	this.SelectedValue = function(value) {
+		if(value===undefined) {
+			return selectedValue;
+		} else {
+			selectedValue = value;
+			return this;
+		};
+	};
+});
